427ac86f1be81198a308978a6a5bbbfd7fbe7528
alffy007 <jamesbond006aj@gmail.com>
2022-12-24 11:31:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import './pages/home'
+import Home from './pages/home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+     <Home/>
+    </div>
+  )
+}
+
+export default App
